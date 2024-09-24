272003c739f5a8df79db1df36bb6d67309a797ab
Bryan Suello <bwsuello@gmail.com>
2024-09-25 01:49:39 +0800

    UPDATE: 9-25-24

--- fish/config.fish
+++ fish/config.fish
@@ -1,46 +1,51 @@
+##### 9-25-24
+##### INSTALL
+# install omf, bobthefish as theme
+# chsh -s /usr/bin/fish -- set fish as default
+# echo $SHELL -- check which shell you are using
+
 set -U fish_greeting "Succeed or Die Trying"
 
-# ALIASES
+##### ALIASES
 
 #alias ls="ls -lah --color=auto"
 alias ls="exa -lah --icons --group-directories-first"
 alias cat="batcat"  #instead of bat
 alias vim="lvim"
 alias cl="clear"
-#alias tm="tmux" #toolazy
 alias b="bash"
 alias r="ranger"
-# alias mountssd="sudo mount -o remount,rw /media/suellobw/SSD/"
 alias remap="xmodmap ~/Xmodmap"
 alias ncal="ncal -y"
+# alias mountssd="sudo mount -o remount,rw /media/suellobw/SSD/"
 
-# utilities
+### utilities
 alias netspeed="speedtest"
-#check 'packet lost, 0 means there's connection
 alias net="ping -c 3 google.com" 
+#check 'packet lost, 0 means there's connection
 # alias battery="cat /sys/class/power_supply/BAT0/capacity"
 
-# locations
-# laptop(popOS)
+### locations
 alias 1="cd ~/Dev/"
-# desktop(ubuntu)
-# alias 1="cd ~/Documents/dev/"
 alias 2="cd ~/.config/"
 alias 3="cd /mnt/c/Documents\ and\ Settings/Brys/Documents/Obsidian_Win"
+# laptop(popOS)
+# desktop(ubuntu)
+# alias 1="cd ~/Documents/dev/"
 # alias downloads="cd ~/Downloads/"
 # alias documents="cd ~/Documents/"
 #alias obsidian="cd /mnt/c/Obsidian_Win/"
 #alias ssd="cd /mnt/x/Downloads\ \(SSD\)/"
 #alias play="node ~/dev/notes/terminalQuizG/app.js"
 
-#xampp
+### xampp
 alias xampp="sudo /opt/lampp/./manager-linux-x64.run"
 alias htdocs="cd /opt/lampp/htdocs/"
 
-##lvim path
+### lvim path
 set -U fish_user_paths ~/.local/bin $fish_user_paths
 
-## let bobthefish prompt used nerdfonts
+### let bobthefish prompt used nerdfonts
 set -g theme_nerd_fonts yes
 set -g theme_display_user ssh
 set -g theme_display_hostname ssh
@@ -51,4 +56,3 @@ export HOST=localhost
 
 # Set starship as prompt
 # starship init fish | source
-
